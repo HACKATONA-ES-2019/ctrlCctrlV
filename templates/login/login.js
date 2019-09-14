0fc1fcadb264c98cd78e7dfd7e576d76a148e5a2
luilencina <luizalencina@hotmail.com>
2019-09-14 20:33:16 -0300

    uploading pages for use on the server side

---
+++ templates/login/login.js
@@ -0,0 +1,6 @@
+function batata() {
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('passw').value;
+
+    console.log(usuario, senha);
+};
