4d71a9ee63decbea502112fda2c4a41b9ad2368f
jojo0severo <joaovbsevero@gmail.com>
2019-09-15 07:38:42 -0300

    ready

--- static/scripts/quiz.js
+++ static/scripts/quiz.js
@@ -3,3 +3,15 @@ function load_quiz(quiz_title){
     let url = "http://127.0.0.1:5000/quiz/" + quiz_title.replace(' ', '_');
     window.location.replace(url);
 }
+
+function change_color(button){
+    let value = button.getAttribute('data-correct');
+
+    if (value == 0){
+        button.style.background = "#e70000";
+    }
+    else{
+        button.style.background = "#89fe96";
+    }
+
+}
